8b1d37b1d31df4b0b5940a51b0b24045222da2e4
Izaskun Mallona <izaskun.mallona@gmail.com>
2018-04-11 12:05:43 +0200

    Parallel cutadapts

--- preliminar.sh
+++ preliminar.sh
@@ -18,7 +18,14 @@ BAUBEC="Group Baubec"
 
 # taupo soft
 FASTQC=/usr/local/software/FastQC/fastqc
-SICKLE="$HOME"/sickle/sickle-1.33/sickle
+SICKLE="$HOME"/soft/sickle/sickle-1.33/sickle
+##CUTADAPT=/usr/local/bin/cutadapt
+CUTADAPT="$VIRTENVS"/cutadapt/bin/cutadapt
+
+# from /usr/local/software/FastQC/Configuration/adapter_list.txt
+ILLUMINA_UNIVERSAL="AGATCGGAAGAG"
+ILLUMINA="CGGTTCAGCAGGAATGCCGAGATCGGAAGAGCGGTT"
+
 
 mkdir -p $WD $BAUBECMOUNT
 cd $WD
@@ -81,42 +88,114 @@ do
 done
 
 ## needed some trimming and adaptor cutting
+## some tiles are odd, and the c/g content indicates a lot of differences in meth...
 
-# no adaptors found for the single end stuff, let's just sickle it
+
+
+# no adaptors found for the single end stuff, we could just sickle it
 # file:///home/imallona/cg_context/SRR1653150_1/SRR1653150_1_fastqc.html
+## but we'll do to keep consistency
+
+source "$VIRTENVS"/cutadapt/bin/activate
+
+ILLUMINA_UNIVERSAL="AGATCGGAAGAG"
+ILLUMINA="CGGTTCAGCAGGAATGCCGAGATCGGAAGAGCGGTT"
+
+TASK="cg_context"
+WD=/home/imallona/"$TASK"
+DATA="$HOME"/data
+SOFT="$HOME"/soft
+
+NTHREADS=6
+
+USER="Izaskun Mallona"
+BAUBECMOUNT=/home/imallona/mnt/baubec
+BAUBEC="Group Baubec"
+
+# taupo soft
+FASTQC=/usr/local/software/FastQC/fastqc
+SICKLE="$HOME"/soft/sickle/sickle-1.33/sickle
+##CUTADAPT=/usr/local/bin/cutadapt
+
+sample=SRR1653150
+# -j$NTHREADS \ ## aparently this version does not allow multithreading
+
+cutadapt \
+    -j $NTHREADS \
+    -b $ILLUMINA_UNIVERSAL -b $ILLUMINA \
+    -o "$WD"/"${sample}"_1_cutadapt.fastq.gz \
+    "$DATA"/"$sample"_1.fastq.gz &> "$WD"/"$sample"_1_cutadapt.log 
 
 
 "$SICKLE" se \
-          -c "$DATA"/SRR1653150_1.fastq.gz \
+          -f "$WD"/"$sample"_1_cutadapt.fastq.gz \
           -g \
           -t sanger \
-          -m "$WD"/SRR1653150_1_sickle.fastq.gz &> "${WD}"/SRR1653150_1_sickle.log
+          -o "$WD"/"$sample"_1_cutadapt_sickle.fastq.gz &> "${WD}"/"$sample"_1_cutadapt_sickle.log
+
+curr="$sample"_1_cutadapt_sickle
+mkdir -p "$curr"
+
+$FASTQC "$WD"/${sample}_1_cutadapt_sickle.fastq.gz \
+        --outdir "$WD"/"$curr" \
+        -t $NTHREADS &> ${curr}/${sample}_1_fastqc.log
 
 
+## run till here #########
+
+echo  "remove the cutadapted tmp file here!"
+
+
+NTHREADS=2
 ## paired end stuff
+for sample in SRR2878513 20151223.B-MmES_TKOD3A1c1-3_R 
+do
+    # for read in 1 2
+    # do
+    cutadapt \
+        -j $NTHREADS \
+        -b $ILLUMINA_UNIVERSAL -b $ILLUMINA \
+        -o "$WD"/"${sample}"_1_cutadapt.fastq.gz \
+        -p "$WD"/"${sample}"_2_cutadapt.fastq.gz \
+        "$DATA"/"$sample"_1.fastq.gz "$DATA"/"$sample"_2.fastq.gz &> "$WD"/"$sample"_cutadapt.log 
+
+    "$SICKLE" pe \
+              --pe-file1 "$WD"/"$sample"_1_cutadapt.fastq.gz \
+              --pe-file2 "$WD"/"$sample"_2_cutadapt.fastq.gz \
+              --output-file1 "$WD"/"$sample"_1_cutadapt_sickle.fastq.gz \
+              --output-file2 "$WD"/"$sample"_2_cutadapt_sickle.fastq.gz \
+              -g \
+              -t sanger &> "$WD"/"$sample"_cutadapt_sickle.log
+
+    for read in 1 2
+    do
+        curr="$sample"_cutadapt_sickle_"$read"
+        mkdir -p "$WD"/$curr
+        $FASTQC "$WD"/${sample}_"$read"_cutadapt_sickle.fastq.gz \
+                --outdir "$WD"/"$curr" \
+                -t $NTHREADS &> ${curr}/${sample}_"$read"_fastqc.log
+
+    done
+    # done
+done
 
 
-# currd=$(basename $1)
-sample=$(basename $1 .fastq)
-echo "Temptative adaptor trimming, just the illumina adaptor"
+echo sickle
 
-cutadapt \
-    -b $ILLUMINA \
-    -o "$TMP"/"${sample}_cutadapted.fastq" \
-    -m $MIN_LENGTH \
-    --discard-trimmed \
-    $(dirname $1)/"$sample"".fastq" &> "${TMP}/${sample}_cutadapt.log" 
 
-echo "Sickle"
+## till here
 
-"$SICKLE" pe \
-          -c "${TMP}/${sample}_cutadapted.fastq" \
-          -g \
-          -t sanger \
-          -m "${TMP}/${sample}_sickle_combined" \
-          -s "$TMP"/"$sample"_sickle_singles_trimmed_file &> "${TMP}/${sample}_sickle.log" 
 
-echo "Fastqc"
 
-"$FASTQC" "${TMP}/${sample}_sickle_combined" -outdir "${TMP}" \
-                               -t $NTHREADS &> "${TMP}/${sample}_fastq.log"
+
+# "$SICKLE" pe \
+#           -c "${TMP}/${sample}_cutadapted.fastq" \
+#           -g \
+#           -t sanger \
+#           -m "${TMP}/${sample}_sickle_combined" \
+#           -s "$TMP"/"$sample"_sickle_singles_trimmed_file &> "${TMP}/${sample}_sickle.log" 
+
+# echo "Fastqc"
+
+# "$FASTQC" "${TMP}/${sample}_sickle_combined" -outdir "${TMP}" \
+#                                -t $NTHREADS &> "${TMP}/${sample}_fastq.log"
